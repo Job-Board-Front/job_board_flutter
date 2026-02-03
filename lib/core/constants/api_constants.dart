6a5473f7e05b0dbe60f7028a9d2c8c1135905acc
Louey Sioua <115192442+loueysioua@users.noreply.github.com>
2026-02-03 11:22:10 +0100

    Update api_constants.dart

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants {
-  //static const String baseUrl = 'http://localhost:3000/api';
-  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String baseUrl = 'http://localhost:3000/api';
+  //static const String baseUrl = 'http://10.0.2.2:3000/api';
 }
